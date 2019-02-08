1c1976854e1fc5d7a4c0d245fc530251284ad3d8
ChesterHu <huchufeng@g.ucla.edu>
2019-02-08 15:44:12 -0500

    read graph in edgelist format

--- scripts/time-measure.py
+++ scripts/time-measure.py
@@ -9,15 +9,15 @@ randFile = "time-rand.txt"
 normFile = "time-norm.txt"
 randqFile = "q-rand.txt"
 normqFile = "q-norm.txt"
-DatasetName = "ca-GrQc-cc"
+DatasetName = "JohnsHopkins"
 
 ref_node = [3]
 alphas = np.linspace(0.1, 0.5, 50)
 rhos = np.logspace(-4, -7, 50)
-epsilons = np.logspace(-2, -4, 50)
+epsilons = np.logspace(-2, -6, 50)
 
 print("loading graph...")
-g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/{0}.graphml".format(DatasetName), "graphml")
+g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/{0}.edgelist".format(DatasetName))
 
 def measureTimeEpsilon(methodName):
     for eps in epsilons:
@@ -35,7 +35,7 @@ def measureTimeRho(methodName):
         lgc.approximate_PageRank(g, ref_node, rho = rho, method = methodName)
 
 # read time file
-def readTimes(fname) -> list:
+def readTimes(fname):
     with open(fname, 'r') as file:
         return np.array(file.read().splitlines(), dtype = np.float)
 
@@ -69,7 +69,7 @@ def plotTime(fname, measureTime, x, label):
     plt.close()
 
 if __name__ == "__main__":
-    plotTime("../figures/run-time-eps-{}-seed{}.png".format(DatasetName, ref_node[0][0]), measureTimeEpsilon, epsilons, "epsilon")
-    plotTime("../figures/run-time-alpha-{}-seed{}.png".format(DatasetName, ref_node[0][0]), measureTimeAlpha, alphas, "alpha")
-    plotTime("../figures/run-time-rho-{}-seed{}.png".format(DatasetName, ref_node[0][0]), measureTimeRho, rhos, "rho")
+    plotTime("../figures/run-time-eps-{}-seed{}.png".format(DatasetName, ref_node[0]), measureTimeEpsilon, epsilons, "epsilon")
+    plotTime("../figures/run-time-alpha-{}-seed{}.png".format(DatasetName, ref_node[0]), measureTimeAlpha, alphas, "alpha")
+    plotTime("../figures/run-time-rho-{}-seed{}.png".format(DatasetName, ref_node[0]), measureTimeRho, rhos, "rho")
 
