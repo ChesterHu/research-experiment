d56cc6ab472c6f0b4287518ecf2fae6336f8a79b
ChesterHu <huchufeng@g.ucla.edu>
2019-03-11 13:58:08 -0400

    test multiple seed nodes

--- scripts/test.py
+++ scripts/test.py
@@ -1,14 +1,20 @@
+import time
 import localgraphclustering as lgc
 
 alpha = 0.15
-eps = 1e-3
-rho = 0.00001
-ref_node = [100]
+eps = 1e-6
+rho = 0.0001
+ref_node = [x for x in range(10)]
 
 print("loading graph...")
 g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
-res = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
-print(res)
+t = time.clock()
+nodes, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
+print(f'non-randomized algorithm uses time: {time.clock() - t}')
+print(nodes)
 
-res = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
-print(res)
+g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
+t = time.clock()
+nodes, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
+print(f'randomized algorithm uses time: {time.clock() - t}')
+print(nodes)
