ca59cb307d3cf44357141bc75da0835188e6dd9e
ChesterHu <huchufeng@g.ucla.edu>
2019-03-11 17:14:10 -0400

    test warm start

--- scripts/test.py
+++ scripts/test.py
@@ -18,3 +18,15 @@ t = time.clock()
 nodes, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
 print(f'randomized algorithm uses time: {time.clock() - t}')
 print(nodes)
+
+# test warm start
+print('test warm start')
+g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
+nodes1, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
+# start after non-randomized method
+nodes2, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
+nodes3, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
+
+print(f'First converge:\n{nodes1}')
+print(f'Second converge:\n{nodes2}')
+print(f'Third converge:\n{nodes3}')
