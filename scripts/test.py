3cf1e4fc1c9e838bc074168376a57eed18c14fe7
ChesterHu <huchufeng@g.ucla.edu>
2019-03-25 15:52:00 -0400

    fix bugs

--- scripts/test.py
+++ scripts/test.py
@@ -5,40 +5,18 @@ import matplotlib.pyplot as plt
 
 import localgraphclustering as lgc
 
-"""
-print("loading graph...")
-g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
-t = time.clock()
-nodes, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
-print(f'non-randomized algorithm uses time: {time.clock() - t}')
-print(nodes)
-
-g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
-t = time.clock()
-nodes, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
-print(f'randomized algorithm uses time: {time.clock() - t}')
-print(nodes)
-
-# test warm start
-print('test warm start')
-g = lgc.GraphLocal("../../LocalGraphClustering/notebooks/datasets/Colgate88_reduced.graphml", "graphml")
-nodes1, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
-# start after non-randomized method
-nodes2, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg-rand")
-nodes3, gradients = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = "l1reg")
-
-print(f'First converge:\n{nodes1}')
-print(f'Second converge:\n{nodes2}')
-print(f'Third converge:\n{nodes3}')
-"""
+alpha = 0.05
+rho = 0.0001
+ref_node = [x for x in range(10)]
 
 def test_time(test_method, epsilons, graph_name):
     """
     Test time for method
     """
-    alpha = 0.15
-    rho = 0.0001
-    ref_node = [x for x in range(10)]
+    global alpha
+    global rho
+    global ref_node
+
     times = []
     g = lgc.GraphLocal(f"../../LocalGraphClustering/notebooks/datasets/{graph_name}.graphml", "graphml")
     for eps in epsilons:
@@ -48,6 +26,9 @@ def test_time(test_method, epsilons, graph_name):
     return times
 
 def plot_run_time(epsilons, graph_name, rand_time, norm_time):
+    global alpha
+    global rho
+
     plt.title(f'Running time on graph {graph_name}')
     plt.semilogx(epsilons, rand_time, label='random')
     plt.semilogx(epsilons, norm_time, label='non-random')
@@ -56,22 +37,23 @@ def plot_run_time(epsilons, graph_name, rand_time, norm_time):
     plt.ylabel('Time (ms)')
 
     plt.legend()
-    fname = f'../figures/run-time-eps-{graph_name}.png'
+    fname = f'../figures/run-time-eps-{graph_name}_alpha={alpha}_rho={rho}.png'
     if os.path.isfile(fname):
         print(f'file: {fname} already exists')
     plt.savefig(fname)
     plt.close()
 
 def compare_result(graph_name):
-    alpha = 0.15
+    global alpha
+    global rho
+    global ref_node
+
     eps = 1e-6
-    rho = 0.0001
-    ref_node = [x for x in range(10)]
     g = lgc.GraphLocal(f"../../LocalGraphClustering/notebooks/datasets/{graph_name}.graphml", "graphml")
     nodes, _ = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = 'l1reg')
     print(f'results from non-random method:\n{nodes}')
     nodes, _ = lgc.approximate_PageRank(g, ref_node, alpha = alpha, rho = rho, epsilon = eps, method = 'l1reg-rand')
-    print(f'results from non-random method:\n{nodes}')
+    print(f'results from random method:\n{nodes}')
 
 if __name__ == "__main__":
     graph_name = 'JohnsHopkins'
