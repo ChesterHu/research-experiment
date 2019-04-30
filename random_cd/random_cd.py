e1be35e8495439ca699a74a38c63506a625e2a66
ChesterHu <huchufeng@g.ucla.edu>
2019-04-30 16:31:29 -0400

    todo: update gradient

--- random_cd/random_cd.py
+++ random_cd/random_cd.py
@@ -1,26 +1,53 @@
 import os
 import numpy as np
 
+from random import randint
+
 import localgraphclustering as lgc
 
 class RandomCD(object):
     def __init__(self):
         self.g = None
-        self.q = []
-        self.gradients = []
-        self.candidates = set()
 
     def load_graph(self, fname):
         self.g = lgc.GraphLocal(fname, 'edgelist', ' ')
 
     def solve(self, ref_node, alpha = 0.15, epsilon = 1e-4, rho = 1e-4):
+        # data structures
+        q = np.zeros(self.g._num_vertices, dtype = float)
+        s = np.zeros(self.g._num_vertices, dtype = float)
+        gradients = np.zeros(self.g._num_vertices, dtype = float)
+        candidates = []
+
+        # setup values, equal to step 1 of ISTA
+        for node in ref_node:
+            s[node] = 1.0 / len(ref_node)
+            gradients[node] = -alpha * self.g.dn_sqrt[node] * s[node]
+            self.update_candidates(node, rho, alpha, q, gradients, candidates)
+        
+        # setps from 2 to 9 of ISTA algorithm
+        threshold = (1 + epsilon) * rho * alpha
+        while not self.is_terminate(gradients, threshold):
+            node = self.sample(candidates)
+            self.update_gradients(node, q, gradients)
+        return (q, gradients)
+
+    def sample(self, candidates):
+        return randint(0, len(candidates))
+
+    def update_gradients(self, node, q, gradients):
         pass
 
-    def update_gradients(self):
-        pass
+    def update_candidates(self, node, rho, alpha, q, gradients, candidates):
+        if node not in candidates and q[node] - gradients[node] >= rho * alpha * self.g.d_sqrt[node]:
+            candidates.append(node)
 
-    def update_candidates(self):
-        pass
+    def is_terminate(self, gradients, threshold):
+        max_norm = 0
+        for node in range(self.g._num_vertices):
+            max_norm = max(max_norm, abs(self.g.dn_sqrt[node] * gradients[node]))
+        return max_norm <= threshold
+
 
 
 if __name__ == "__main__":
