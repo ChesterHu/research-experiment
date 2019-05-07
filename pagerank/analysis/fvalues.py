9be216d308638ef990e2018a08d2b913e679fbab
ChesterHu <huchufeng@g.ucla.edu>
2019-05-07 14:26:36 -0400

    test faster version of accelerated method, simplify code

--- pagerank/analysis/fvalues.py
+++ pagerank/analysis/fvalues.py
@@ -3,49 +3,66 @@ This script produce plot for comparing function values in both iteration.
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 
 from pagerank.random_cd import RandomCD
 from pagerank.accelerate_cd import AccelerateCD
 from pagerank.accelerate_cd_fast import AccelerateCDFast
+from pagerank.test_config import TestConfig
 
-full_path = os.path.realpath(__file__)
-dir_name = os.path.dirname(full_path)
-graph_file = f'{dir_name}/data/ppi_mips.graphml'
-graph_type = 'graphml'
+def plot_fvalues(solver, config, linestyle = 'solid', color = 'red'):
+    solver.load_graph(config.graph_file, graph_type)
+    _, fvalues, __ = solver.solve(config.ref_nodes, config.alpha, config.rho, config.epsilon, config.max_iter)
+    iterations = [i for i in range(1, len(fvalues) + 1)]
+    plt.plot(iterations, fvalues, label = str(solver), linestyle = linestyle, color = color)
 
-# experiment parameters
-ref_nodes = [4]
-alpha = 0.1
-rho = 1e-4
-epsilon = 1e-8
-max_iter = 1000
+if __name__ == "__main__":
 
-def plot_fvalues(solver, config):
-    pass
+    # experiment parameters
+    ref_nodes = [4]
+    alpha = 0.1
+    rho = 1e-4
+    epsilon = 1e-8
+    max_iter = 1000
+    graph_type = 'graphml'
+    graph_file = 'ppi_mips.graphml'
+    config = TestConfig(ref_nodes, alpha, rho, epsilon, max_iter, graph_file, graph_type)
 
-# solve accelerated method
-accel_solver = AccelerateCD()
-accel_solver.load_graph(graph_file, graph_type)
-accel_q, accel_fvalues, accel_nzeros = accel_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-# solve non-accelerated method
-rand_solver = RandomCD()
-rand_solver.load_graph(graph_file, graph_type)
-rand_q, rand_fvalues, rand_nzeros = rand_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-# solve accelerated method fast version
-fast_solver = AccelerateCDFast()
-fast_solver.load_graph(graph_file, graph_type)
-fast_q, fast_fvalues, fast_nzeros = fast_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
+    # solve
+    plot_fvalues(AccelerateCD(), config, linestyle = 'solid', color = 'red')
+    plot_fvalues(AccelerateCDFast(), config, linestyle = 'dotted', color = 'black')
+    plot_fvalues(RandomCD(), config, linestyle = 'dashed', color = 'green')
 
-# plot function value in both methods
-plt.plot([_ + 1 for _ in range(len(accel_fvalues))], accel_fvalues, label = 'accelerated method', linestyle = 'solid', linewidth = 3, color = 'red')
-plt.plot([_ + 1 for _ in range(len(rand_fvalues))], rand_fvalues, label = 'non-accelerated method', linestyle = 'dashed', linewidth = 3, color = 'black')
-plt.plot([_ + 1 for _ in range(len(fast_fvalues))], fast_fvalues, label = 'fast accelerated method', linestyle = 'dotted', linewidth = 3, color = 'green')
+    # plot
+    fontsize = 20
+    legendsize = 20
+    plt.legend(prop = {'size': legendsize})
+    plt.xlabel('iterations', fontsize = fontsize)
+    plt.ylabel('function vales', fontsize = fontsize)
+    plt.xscale('log')
+    plt.show()
+    """
+    # solve accelerated method
+    accel_solver = AccelerateCD()
+    accel_solver.load_graph(graph_file, graph_type)
+    accel_q, accel_fvalues, accel_nzeros = accel_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
+    # solve non-accelerated method
+    rand_solver = RandomCD()
+    rand_solver.load_graph(graph_file, graph_type)
+    rand_q, rand_fvalues, rand_nzeros = rand_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
+    # solve accelerated method fast version
+    fast_solver = AccelerateCDFast()
+    fast_solver.load_graph(graph_file, graph_type)
+    fast_q, fast_fvalues, fast_nzeros = fast_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
 
-# plot settings
-plt.xscale('log')
-plt.legend(prop={'size': 18})
-plt.xlabel('iterations', fontsize = 20)
-plt.ylabel('function value', fontsize = 20)
-plt.show()
+    # plot function value in both methods
+    plt.plot([_ + 1 for _ in range(len(accel_fvalues))], accel_fvalues, label = str(accel_solver), linestyle = 'solid', linewidth = 3, color = 'red')
+    plt.plot([_ + 1 for _ in range(len(rand_fvalues))], rand_fvalues, label = str(rand_solver), linestyle = 'dashed', linewidth = 3, color = 'black')
+    plt.plot([_ + 1 for _ in range(len(fast_fvalues))], fast_fvalues, label = str(fast_solver), linestyle = 'dotted', linewidth = 3, color = 'green')
+
+    # plot settings
+    plt.xscale('log')
+    plt.legend(prop={'size': 18})
+    plt.xlabel('iterations', fontsize = 20)
+    plt.ylabel('function value', fontsize = 20)
+    plt.show()
+    """
