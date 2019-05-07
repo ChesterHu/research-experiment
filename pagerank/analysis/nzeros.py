9d80ae295d35a4d0c49978e95d171b6271a648b9
ChesterHu <huchufeng@g.ucla.edu>
2019-05-07 15:44:14 -0400

    rewrite code

--- pagerank/analysis/nzeros.py
+++ pagerank/analysis/nzeros.py
@@ -2,44 +2,42 @@
 This script produce plot for comparing number of non-zero nodes in both methods. 
 The accelerated method can touch more non-zero nodes than non-accelerated method, but it uses less iterations to converge.
 """
-
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 
 from pagerank.random_cd import RandomCD
 from pagerank.accelerate_cd import AccelerateCD
+from pagerank.accelerate_cd_fast import AccelerateCDFast
+from pagerank.test_config import TestConfig
 
-full_path = os.path.realpath(__file__)
-dir_name = os.path.dirname(full_path)
-graph_file = f'{dir_name}/data/ppi_mips.graphml'
-graph_type = 'graphml'
-separator = ''
+def plot_nzeros(solver, config, linestyle = 'solid', color = 'red'):
+    solver.load_graph(config.graph_file, graph_type)
+    _, __, nzeros = solver.solve(config.ref_nodes, config.alpha, config.rho, config.epsilon, config.max_iter)
+    iterations = [i for i in range(1, len(nzeros) + 1)]
+    plt.plot(iterations, nzeros, label = str(solver), linestyle = linestyle, linewidth = 3, color = color)
+    plt.axhline(y = nzeros[-1], linestyle = 'dashdot', linewidth = 3, color = 'blue')
 
-# experiment parameters
-ref_nodes = [4]
-alpha = 0.1
-rho = 1e-4
-epsilon = 1e-8
-max_iter = 1000
+if __name__ == "__main__":
 
-# solve accelerated method
-accel_solver = AccelerateCD()
-accel_solver.load_graph(graph_file, graph_type)
-accel_q, accel_fvalues, accel_nzeros = accel_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-# solve non-accelerated method
-rand_solver = RandomCD()
-rand_solver.load_graph(graph_file, graph_type, separator)
-rand_q, rand_fvalues, rand_nzeros = rand_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
+    # experiment parameters
+    ref_nodes = [4]
+    alpha = 0.1
+    rho = 1e-4
+    epsilon = 1e-8
+    max_iter = 1000
+    graph_type = 'graphml'
+    graph_file = 'ppi_mips.graphml'
+    config = TestConfig(ref_nodes, alpha, rho, epsilon, max_iter, graph_file, graph_type)
 
-# plot number of non-zero nodes in both methods and horizontal line for number of non-zero nodes in optimal solution
-plt.plot([_ + 1 for _ in range(len(accel_nzeros))], accel_nzeros, label = 'accelerated method', linestyle = 'solid', linewidth = 3, color = 'red')
-plt.plot([_ + 1 for _ in range(len(rand_nzeros))], rand_nzeros, label = 'non-accelerated method', linestyle = 'dashed', linewidth = 3, color = 'black')
-plt.axhline(y = len(np.nonzero(rand_q)[0]), label = 'optimal solution', linestyle = 'dotted', linewidth = 3, color = 'blue')
+    # solve
+    plot_nzeros(AccelerateCD(), config, linestyle = 'solid', color = 'red')
+    plot_nzeros(AccelerateCDFast(), config, linestyle = 'dotted', color = 'black')
+    plot_nzeros(RandomCD(), config, linestyle = 'dashed', color = 'green')
 
-# plot settings
-plt.xscale('log')
-plt.legend(prop={'size': 18})
-plt.xlabel('iterations', fontsize = 20)
-plt.ylabel('number of non-zero nodes', fontsize = 20)
-plt.show()
+    # plot
+    fontsize = 20
+    legendsize = 20
+    plt.legend(prop = {'size': legendsize})
+    plt.xlabel('iterations', fontsize = fontsize)
+    plt.ylabel('number of non-zero nodes', fontsize = fontsize)
+    plt.xscale('log')
+    plt.show()
