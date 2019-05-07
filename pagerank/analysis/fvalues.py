11b3723fc1c31f93a9383f7e66fe7dedf46f5e0f
ChesterHu <huchufeng@g.ucla.edu>
2019-05-07 14:31:11 -0400

    remove old code;

--- pagerank/analysis/fvalues.py
+++ pagerank/analysis/fvalues.py
@@ -40,29 +40,3 @@ if __name__ == "__main__":
     plt.ylabel('function vales', fontsize = fontsize)
     plt.xscale('log')
     plt.show()
-    """
-    # solve accelerated method
-    accel_solver = AccelerateCD()
-    accel_solver.load_graph(graph_file, graph_type)
-    accel_q, accel_fvalues, accel_nzeros = accel_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-    # solve non-accelerated method
-    rand_solver = RandomCD()
-    rand_solver.load_graph(graph_file, graph_type)
-    rand_q, rand_fvalues, rand_nzeros = rand_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-    # solve accelerated method fast version
-    fast_solver = AccelerateCDFast()
-    fast_solver.load_graph(graph_file, graph_type)
-    fast_q, fast_fvalues, fast_nzeros = fast_solver.solve(ref_nodes, alpha, rho, epsilon, max_iter)
-
-    # plot function value in both methods
-    plt.plot([_ + 1 for _ in range(len(accel_fvalues))], accel_fvalues, label = str(accel_solver), linestyle = 'solid', linewidth = 3, color = 'red')
-    plt.plot([_ + 1 for _ in range(len(rand_fvalues))], rand_fvalues, label = str(rand_solver), linestyle = 'dashed', linewidth = 3, color = 'black')
-    plt.plot([_ + 1 for _ in range(len(fast_fvalues))], fast_fvalues, label = str(fast_solver), linestyle = 'dotted', linewidth = 3, color = 'green')
-
-    # plot settings
-    plt.xscale('log')
-    plt.legend(prop={'size': 18})
-    plt.xlabel('iterations', fontsize = 20)
-    plt.ylabel('function value', fontsize = 20)
-    plt.show()
-    """
