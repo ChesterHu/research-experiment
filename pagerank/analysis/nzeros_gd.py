f59cc22f1b792513eba36a698f6aaf562c2119fa
ChesterHu <huchufeng@g.ucla.edu>
2019-05-13 14:58:51 -0400

    script to test gradient descent

---
+++ pagerank/analysis/nzeros_gd.py
@@ -0,0 +1,10 @@
+"""
+Test non-zero nodes in gradient descent and accelerated gradient descent
+"""
+import matplotlib.pyplot as plt
+
+from nzeros import plot_nzeros
+from pagerank.accelerate_gd import AccelerateGD
+
+if __name__ == "__main__":
+    pass
