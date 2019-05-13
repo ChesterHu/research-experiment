9d04c9a221e3ce61e0dce9b73deb785717b6832e
ChesterHu <huchufeng@g.ucla.edu>
2019-05-13 16:19:07 -0400

    fix bugs

--- pagerank/accelerate_gd.py
+++ pagerank/accelerate_gd.py
@@ -12,7 +12,6 @@ class AccelerateGD(PageRank):
     def optimize(self, alpha, rho, epsilon, max_iter, q, s, candidates, gradients, fvalues, nzeros, times):
         y = np.zeros(self.g._num_vertices, dtype = float)
         prev_q = np.zeros(self.g._num_vertices, dtype = float)
-        nzero_nodes = set(candidates)
 
         fvalues.append(self.compute_fvalue(alpha, rho, q, s))
         times.append(0)
@@ -25,14 +24,15 @@ class AccelerateGD(PageRank):
             q, prev_q = prev_q, q
             beta = self.compute_beta(num_iter, alpha)
             self.update_q(alpha, rho, q, y, gradients)
-            self.update_y(beta, q, prev_q, y, nzero_nodes)
-            self.update_gradients(alpha, rho, y, s, gradients, nzero_nodes)
+            # better implementation should keep track of non-zero nodes and seed nodes.
+            self.update_y(beta, q, prev_q, y)
+            self.update_gradients(alpha, rho, y, s, gradients)
             
             dt += time.time() - st
             times.append(dt * 1000)
             print(times[-1])
             fvalues.append(self.compute_fvalue(alpha, rho, q, s))
-            nzeros.append(len(nzero_nodes))
+            nzeros.append(len(np.nonzero(q)[0]))
             st = time.time()
 
 
@@ -54,16 +54,14 @@ class AccelerateGD(PageRank):
             else:
                 q[node] = 0
 
-    def update_y(self, beta, q, prev_q, y, nzero_nodes):
+    def update_y(self, beta, q, prev_q, y):
         for node in range(self.g._num_vertices):
             y[node] = q[node] + beta * (q[node] - prev_q[node])
-            if y[node] != 0:
-                nzero_nodes.add(node)
 
-    def update_gradients(self, alpha, rho, y, s, gradients, nzero_nodes):
-        for node in nzero_nodes:
+    def update_gradients(self, alpha, rho, y, s, gradients):
+        for node in range(self.g._num_vertices):
             gradients[node] = 0.5 * (1 + alpha) * y[node] - alpha * s[node] * self.g.dn_sqrt[node]
         
-        for node in nzero_nodes:
+        for node in range(self.g._num_vertices):
             for neighbor in self.g.neighbors(node):
                 gradients[neighbor] -= 0.5 * (1 - alpha) * y[node] * self.g.dn_sqrt[node] * self.g.dn_sqrt[neighbor]
