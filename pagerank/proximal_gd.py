3d740205781d3acca29153e1969721fa9894aea5
ChesterHu <huchufeng@g.ucla.edu>
2019-05-13 16:32:57 -0400

    simple implementation of proximal gradient descent

---
+++ pagerank/proximal_gd.py
@@ -0,0 +1,14 @@
+import numpy as np
+import time
+from math import sqrt
+
+from pagerank.accelerate_gd import AccelerateGD
+
+class ProximalGD(AccelerateGD):
+    
+    def __str__(self):
+        return 'proximal gradient descent'
+
+    def compute_beta(self, num_iter, alpha):
+        # non accelerated method always has beta = 0
+        return 0
