21f7a3eccb2600ff25408ec05ec40c5c2056cc34
ChesterHu <huchufeng@g.ucla.edu>
2019-05-21 11:39:08 -0400

    plot_nzero now accepts plot configurations and loads graph when required

--- pagerank/analysis/nzeros.py
+++ pagerank/analysis/nzeros.py
@@ -10,42 +10,47 @@ from pagerank.accelerate_gd import AccelerateGD
 from pagerank.proximal_gd import ProximalGD
 from pagerank.config import Config
 
-def plot_nzeros(solver, config, linestyle = 'solid', color = 'red'):
-    solver.load_graph(config.graph_file, config.graph_type)
+def plot_nzeros(solver, **kwargs):
+    color = kwargs.get('color', 'red')
+    config = kwargs.get('config', None)
+    fontsize = kwargs.get('fontsize', 20)
+    linestyle = kwargs.get('linestyle', 'solid')
+    linewidth = kwargs.get('linewidth', 3)
+    xscale = kwargs.get('xscale', 'linear')
+
+    # load graph if it's not set
+    if config.graph_file:
+        solver.load_graph(config.graph_file, config.graph_type)
+    elif not solver.g:
+        raise ValueError('graph is empty')
     _, _, nzeros, _ = solver.solve(config.ref_nodes, config.alpha, config.rho, config.epsilon, config.max_iter)
-    iterations = [i for i in range(1, len(nzeros) + 1)]
-    plt.plot(iterations, nzeros, label = str(solver), linestyle = linestyle, linewidth = 3, color = color)
-    plt.axhline(y = nzeros[-1], linestyle = 'dashdot', linewidth = 3, color = 'blue')
+    optimal_nzeros = nzeros[-1]
+    
+    plt.xlabel('iterations', fontsize = fontsize)
+    plt.ylabel('number of non-zero nodes', fontsize = fontsize)
+    plt.xscale(xscale)
+
+    plt.plot(nzeros, label = str(solver), linestyle = linestyle, linewidth = linewidth, color = color)
+    plt.axhline(y = optimal_nzeros, linestyle = 'dashdot', linewidth = 3, color = 'blue')
+
 
 if __name__ == "__main__":
 
     # load configs
     config_file = 'config.yaml'
     config = Config(config_file)
+    legendsize = 20
 
     # coordinate descent
     plt.subplot(1, 2, 1)
-    plot_nzeros(AccelerateCD(), config, linestyle = 'solid', color = 'red')
-    plot_nzeros(RandomCD(), config, linestyle = 'dashed', color = 'black')
-    
-    fontsize = 20
-    legendsize = 20
+    plot_nzeros(AccelerateCD(), linestyle = 'solid', color = 'red', config = config)
+    plot_nzeros(RandomCD(), linestyle = 'dashed', color = 'black', config = config)
     plt.legend(prop = {'size': legendsize})
-    plt.xlabel('iterations', fontsize = fontsize)
-    plt.ylabel('number of non-zero nodes', fontsize = fontsize)
-    plt.xscale('log')
 
     # gradient descent
     plt.subplot(1, 2, 2)
-    config.max_iter = 100 # gradient descent uses less iterations
-    plot_nzeros(AccelerateGD(), config, linestyle = 'solid', color = 'red')
-    plot_nzeros(ProximalGD(), config, linestyle = 'dashed', color = 'black')
-
-    fontsize = 20
-    legendsize = 20
+    config.max_iter = 50 # gradient descent uses less iterations
+    plot_nzeros(AccelerateGD(), linestyle = 'solid', color = 'green', config = config)
+    plot_nzeros(ProximalGD(), linestyle = 'dashed', color = 'purple', config = config)
     plt.legend(prop = {'size': legendsize})
-    plt.xlabel('iterations', fontsize = fontsize)
-    plt.ylabel('number of non-zero nodes', fontsize = fontsize)
-    plt.xscale('log')
-
     plt.show()
