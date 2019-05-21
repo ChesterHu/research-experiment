525d3bf18f5b4a4dda19616287f75e4925c8f2e5
ChesterHu <huchufeng@g.ucla.edu>
2019-05-21 11:32:01 -0400

    set default values if the loading of config file fails

--- pagerank/config.py
+++ pagerank/config.py
@@ -6,23 +6,26 @@ from types import SimpleNamespace
 class Config(object):
 
     def __init__(self, config_file):
-        config_file = self.resolve_conif_fname(config_file)
+        config_file = self.resolve_config_fname(config_file)
         config = yaml.safe_load(open(config_file))
 
-        self.alpha = config['alpha']
-        self.epsilon = config['epsilon']
-        self.rho = config['rho']
-        self.max_iter = config['max_iter']
-        self.ref_nodes = config['ref_nodes']
-        self.graph_file = self.resolve_data_fname(config['graph_file'])
-        self.graph_type = config['graph_type']
+        self.alpha = config.get('alpha', 0.15)
+        self.epsilon = config.get('epsilon', 1e-4)
+        self.rho = config.get('rho', 1e-4)
+        self.max_iter = config.get('max_iter', 100)
+        self.ref_nodes = config.get('ref_nodes', [0])
+
+        self.graph_file = config.get('graph_file', '')
+        self.graph_type = config.get('graph_type', '')
+        if self.graph_file:
+            self.graph_file = self.resolve_data_fname(self.graph_file)
 
 
     def resolve_data_fname(self, fname):
         dir_name = self.resolve_dirname(fname)
         return f'{dir_name}/analysis/data/{fname}'
 
-    def resolve_conif_fname(self, fname):
+    def resolve_config_fname(self, fname):
         dir_name = self.resolve_dirname(fname)
         return f'{dir_name}/analysis/{fname}'
 
