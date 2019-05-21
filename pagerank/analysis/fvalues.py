d8254cd267d0babd9ae1d760f8fd9dced5b2b201
ChesterHu <huchufeng@g.ucla.edu>
2019-05-21 11:48:08 -0400

    plot_fvalues now accept plot parameters and load graph when it is required

--- pagerank/analysis/fvalues.py
+++ pagerank/analysis/fvalues.py
@@ -9,11 +9,27 @@ from pagerank.accelerate_gd import AccelerateGD
 from pagerank.proximal_gd import ProximalGD
 from pagerank.config import Config
 
-def plot_fvalues(solver, config, linestyle = 'solid', color = 'red'):
-    solver.load_graph(config.graph_file, config.graph_type)
+def plot_fvalues(solver, **kwargs):
+    color = kwargs.get('color', 'red')
+    config = kwargs.get('config', None)
+    fontsize = kwargs.get('fontsize', 20)
+    linestyle = kwargs.get('linestyle', 'solid')
+    linewidth = kwargs.get('linewidth', 3)
+    xscale = kwargs.get('xscale', 'log')
+
+    if config.graph_file:
+        solver.load_graph(config.graph_file, config.graph_type)
+    elif not solver.g:
+        raise ValueError('graph is empty')
     _, fvalues, _, _ = solver.solve(config.ref_nodes, config.alpha, config.rho, config.epsilon, config.max_iter)
-    iterations = [i for i in range(1, len(fvalues) + 1)]
-    plt.plot(iterations, fvalues, label = str(solver), linestyle = linestyle, linewidth = 3, color = color)
+    iterations = [i + 1 for i in range(len(fvalues))]
+
+    plt.xlabel('iterations', fontsize = fontsize)
+    plt.ylabel('function value', fontsize = fontsize)
+    plt.xscale(xscale)
+
+    plt.plot(iterations, fvalues, label = str(solver), linestyle = linestyle, linewidth = linewidth, color = color)
+
 
 if __name__ == "__main__":
 
@@ -22,24 +38,18 @@ if __name__ == "__main__":
     config = Config(config_file)
 
     # plot
-    fontsize = 20
     legendsize = 20
 
     plt.subplot(1, 2, 1)
-    plot_fvalues(AccelerateCD(), config, linestyle = 'solid', color = 'red')
-    plot_fvalues(RandomCD(), config, linestyle = 'dotted', color = 'black')
+    plot_fvalues(AccelerateCD(), linestyle = 'solid', color = 'red', config = config)
+    plot_fvalues(RandomCD(), linestyle = 'dotted', color = 'black', config = config)
     plt.legend(prop = {'size': legendsize})
-    plt.xlabel('iterations', fontsize = fontsize)
-    plt.ylabel('function vales', fontsize = fontsize)
-    plt.xscale('log')
+
 
     plt.subplot(1, 2, 2)
     config.max_iter = 100
-    plot_fvalues(ProximalGD(), config, linestyle = 'dashed', color = 'green')
-    plot_fvalues(AccelerateGD(), config, linestyle = 'solid', color = 'purple')
+    plot_fvalues(ProximalGD(), linestyle = 'dashed', color = 'green', config = config)
+    plot_fvalues(AccelerateGD(), linestyle = 'solid', color = 'purple', config = config)
     plt.legend(prop = {'size': legendsize})
-    plt.xlabel('iterations', fontsize = fontsize)
-    plt.ylabel('function vales', fontsize = fontsize)
-    plt.xscale('log')
     
     plt.show()
