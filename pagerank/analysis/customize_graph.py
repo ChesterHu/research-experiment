c533a07af2864dd3909c9ee5ce9f3fddf380071a
ChesterHu <huchufeng@g.ucla.edu>
2019-05-22 14:05:20 -0400

    test on smaller graph

--- pagerank/analysis/customize_graph.py
+++ pagerank/analysis/customize_graph.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import random
 
+from pagerank.accelerate_gd import AccelerateGD
 from pagerank.accelerate_gd_np import AccelerateGDNumpy
 from pagerank.config import Config
 from pagerank.proximal_gd import ProximalGD
+from pagerank.random_cd import RandomCD
 
 from nzeros import plot_nzeros
 from fvalues import plot_fvalues
@@ -24,34 +26,32 @@ def random_edges(ref_nodes, num_nodes, num_edges, max_try = 1000):
     return list(edge_list)
 
 if __name__ == "__main__":
-    '''
-    edge_list = [
-        (0, 1), 
-        (1, 2),
-        (2, 3),
-        (3, 4),
-        (4, 5),
-        (5, 6)
-    ]
-    '''
-    edge_list = []
-    legendsize = 16
-    num_nodes = 100
-    num_edges = 100
-    
-
     config_file = 'config.yaml'
     config = Config(config_file)
     config.max_iter = 20
-    edge_list = random_edges(config.ref_nodes, num_nodes, num_edges)
 
-    solver = AccelerateGDNumpy()
+    edge_list = [
+        (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 10), (6, 8), (7, 10), (5, 7)
+    ]
+    config.rho = 1e-6
+    config.alpha = 0.5
+    
+    '''
+    edge_list = []
+    num_nodes = 100
+    num_edges = 100
+    edge_list = random_edges(config.ref_nodes, num_nodes, num_edges)
+    '''
+
+    legendsize = 16
+    xscale = 'log'
+    solver = AccelerateGD()
     solver.build_graph(edge_list)
-    plot_nzeros(solver, config = config, linestyle = 'solid', color = 'red')
+    plot_nzeros(solver, config = config, linestyle = 'solid', color = 'red', xscale = xscale)
     
     solver = ProximalGD()
     solver.build_graph(edge_list)
-    plot_nzeros(solver, config = config, linestyle = 'dashdot', color = 'black')
-
+    plot_nzeros(solver, config = config, linestyle = 'dashdot', color = 'black', xscale = xscale)
+    
     plt.legend(prop = {'size': legendsize})
     plt.show()
